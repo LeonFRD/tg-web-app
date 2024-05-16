4e4d190414fd7e937e518ee05e8324f137d044b6
Your Name <you@example.com>
2024-05-17 02:02:22 +0300

    Your descriptive commit message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Make sure the path is correct!
 import App from './App';
-import reportWebVitals from './reportWebVitals'; // If you've installed react-scripts
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
